bedceb3efbaf2db8d7b91fe46eb7b0cfe653b65b
Przemysław Rychter <39413606+PrzemekRychter@users.noreply.github.com>
2020-05-07 21:10:57 +0200

    Update insert_disciplines.sql
    zmiana , po dodaniu kolumny discipline

--- Skrypty wypełniające baze/insert_disciplines.sql
+++ Skrypty wypełniające baze/insert_disciplines.sql
@@ -1,62 +1,62 @@
-INSERT INTO disciplines (discipline_id, sex, d_description)
-VALUES ('100M','male','Najkrótszy dystans w biegach lekkoatletycznych na otwartym stadionie 
+INSERT INTO disciplines (discipline, sex, d_description)
+ VALUES ('100m','male','Najkrótszy dystans w biegach lekkoatletycznych na otwartym stadionie 
  (w hali biega się 60 metrów), klasyczny bieg sprinterski. Rekordzistów świata na tym dystansie 
  określa się często mianem „najszybszego mężczyzny/najszybszej kobiety świata”. 
 Rywalizacja na tym dystansie przyciąga zawsze wiele uwagi.'),
-('100F','female',
+('100','female',
 'Najkrótszy dystans w biegach lekkoatletycznych na otwartym stadionie (w hali biega się 60 metrów), 
 klasyczny bieg sprinterski. Rekordzistów świata na tym dystansie określa się często mianem 
 „najszybszego mężczyzny/najszybszej kobiety świata”. 
 Rywalizacja na tym dystansie przyciąga zawsze wiele uwagi.'),
-('200M','male','Bieg sprinterski rozgrywany zarówno na otwartym stadionie, jak i w hali.'),
-('200F','female','Bieg sprinterski rozgrywany zarówno na otwartym stadionie, jak i w hali.'),
-('400M','male','Najdłuższy dystans spośród biegów krótkich. Na standardowym stadionie 400 m 
+('200m','male','Bieg sprinterski rozgrywany zarówno na otwartym stadionie, jak i w hali.'),
+('200m','female','Bieg sprinterski rozgrywany zarówno na otwartym stadionie, jak i w hali.'),
+('400m','male','Najdłuższy dystans spośród biegów krótkich. Na standardowym stadionie 400 m 
 stanowi dokładnie jedno okrążenie stadionu po wewnętrznym torze. W hali jest to dystans dwóch okrążeń.'),
-('400F','female','Najdłuższy dystans spośród biegów krótkich. Na standardowym stadionie 400 m 
+('400m','female','Najdłuższy dystans spośród biegów krótkich. Na standardowym stadionie 400 m 
 stanowi dokładnie jedno okrążenie stadionu po wewnętrznym torze. W hali jest to dystans dwóch okrążeń.'),
-('800M','male','Lekkoatletyczna konkurencja biegowa, zaliczana do biegów średnich. 
+('800m','male','Lekkoatletyczna konkurencja biegowa, zaliczana do biegów średnich. 
 Na otwartym stadionie bieg na 800 metrów to dwa okrążenia stadionu. Pierwsze 100 metrów zawodnicy 
 biegną po swoich torach, po czym mogą zbiec ku wewnętrznemu torowi.'),
-('800F','female','Lekkoatletyczna konkurencja biegowa, zaliczana do biegów średnich. 
+('800m','female','Lekkoatletyczna konkurencja biegowa, zaliczana do biegów średnich. 
 Na otwartym stadionie bieg na 800 metrów to dwa okrążenia stadionu. Pierwsze 100 metrów zawodnicy 
 biegną po swoich torach, po czym mogą zbiec ku wewnętrznemu torowi.'),
-('1500M','male','Lekkoatletyczna konkurencja biegowa, zaliczana do biegów średnich. 
+('1500m','male','Lekkoatletyczna konkurencja biegowa, zaliczana do biegów średnich. 
 Na otwartym stadionie zawodnicy biegną najpierw 300 m odcinek, a później trzy pełne okrążenia stadionu.'),
-('1500F','female','Lekkoatletyczna konkurencja biegowa, zaliczana do biegów średnich. 
+('1500m','female','Lekkoatletyczna konkurencja biegowa, zaliczana do biegów średnich. 
 Na otwartym stadionie zawodnicy biegną najpierw 300 m odcinek, a później trzy pełne okrążenia stadionu.'),
-('5000M','male','Konkurencja lekkoatletyczna, zaliczana do biegów długich. Zawodnicy pokonują 
+('5000m','male','Konkurencja lekkoatletyczna, zaliczana do biegów długich. Zawodnicy pokonują 
 12,5 okrążeń bieżni stadionu bądź 25 okrążeń w hali, choć ten dystans nie jest rozgrywany na 
 najważniejszych imprezach halowych. '),
-('5000F','female','Konkurencja lekkoatletyczna, zaliczana do biegów długich. Zawodnicy pokonują 
+('5000','female','Konkurencja lekkoatletyczna, zaliczana do biegów długich. Zawodnicy pokonują 
 12,5 okrążeń bieżni stadionu bądź 25 okrążeń w hali, choć ten dystans nie jest rozgrywany na 
 najważniejszych imprezach halowych.'),
-('10000M','male','Konkurencja lekkoatletyczna, zaliczana do biegów długich. Zawodnicy
+('10000','male','Konkurencja lekkoatletyczna, zaliczana do biegów długich. Zawodnicy
  pokonują 25 okrążeń bieżni stadionu.'), 
- ('10000F','female','Konkurencja lekkoatletyczna, zaliczana do biegów długich. Zawodnicy
+ ('10000','female','Konkurencja lekkoatletyczna, zaliczana do biegów długich. Zawodnicy
  pokonują 25 okrążeń bieżni stadionu.'),
- ('longJumpM','male','Konkurencja lekkoatletyczna, w której zawodnicy wykonują rozbieg, 
+ ('long jump','male','Konkurencja lekkoatletyczna, w której zawodnicy wykonują rozbieg, 
  odbijają się z jednej nogi z belki i lądują na piaszczystej części skoczni.'),
-('longJumpF','female','Konkurencja lekkoatletyczna, w której zawodnicy wykonują rozbieg, 
+('long jump','female','Konkurencja lekkoatletyczna, w której zawodnicy wykonują rozbieg, 
  odbijają się z jednej nogi z belki i lądują na piaszczystej części skoczni.'),
- ('highJumpM','male','Konkurencja lekkoatletyczna, polegająca na odbiciu się (po wykonaniu rozbiegu) i
+ ('high jump','male','Konkurencja lekkoatletyczna, polegająca na odbiciu się (po wykonaniu rozbiegu) i
  przeniesieniu całego ciała ponad poprzeczką zawieszoną na pionowych stojakach. Zawodnicy mają 
  trzy próby na pokonanie danej wysokości.'), 
-  ('highJumpF','female','Konkurencja lekkoatletyczna, polegająca na odbiciu się (po wykonaniu rozbiegu) i
+  ('high jump','female','Konkurencja lekkoatletyczna, polegająca na odbiciu się (po wykonaniu rozbiegu) i
  przeniesieniu całego ciała ponad poprzeczką zawieszoną na pionowych stojakach. Zawodnicy mają 
  trzy próby na pokonanie danej wysokości.'),
- ('tripleJumpM','male','Trójskok – konkurencja lekkoatletyczna, w której zawodnik musi wykonać trzy
+ ('triple jump','male','Trójskok – konkurencja lekkoatletyczna, w której zawodnik musi wykonać trzy
  następujące po sobie skoki: pierwszy – odbicie z jednej nogi i lądowanie na nogę odbijającą, 
  drugi – lądowanie na nogę przeciwną, trzeci – lądowanie na piaszczystej skoczni obunóż.
  Zasady rozbiegu, odbicia, lądowania i mierzenia są identyczne jak w skoku w dal. Dwa pierwsze skoki 
  odbywają się na twardej bieżni, trzeci skok kończy się na piasku.'),
-  ('tripleJumpF','female','Trójskok – konkurencja lekkoatletyczna, w której zawodnik musi wykonać trzy
+  ('triple jump','female','Trójskok – konkurencja lekkoatletyczna, w której zawodnik musi wykonać trzy
  następujące po sobie skoki: pierwszy – odbicie z jednej nogi i lądowanie na nogę odbijającą, 
  drugi – lądowanie na nogę przeciwną, trzeci – lądowanie na piaszczystej skoczni obunóż.
  Zasady rozbiegu, odbicia, lądowania i mierzenia są identyczne jak w skoku w dal. Dwa pierwsze skoki 
  odbywają się na twardej bieżni, trzeci skok kończy się na piasku.'),
- ('poleVaultM','male','Konkurencja lekkoatletyczna, w której skok odbywa się na takich zasadach 
+ ('pole vault','male','Konkurencja lekkoatletyczna, w której skok odbywa się na takich zasadach 
  jak w skoku wzwyż, z tą różnicą, że zawodnik do pokonania poprzeczki używa tyczki.'),
-  ('poleVaultF','female','Konkurencja lekkoatletyczna, w której skok odbywa się na takich zasadach 
+  ('pole vault','female','Konkurencja lekkoatletyczna, w której skok odbywa się na takich zasadach 
  jak w skoku wzwyż, z tą różnicą, że zawodnik do pokonania poprzeczki używa tyczki.');
  
 
