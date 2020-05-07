9ad4ba21cdea72ba7748b4a2b9121762e80ceaad
Przemysław Rychter <39413606+PrzemekRychter@users.noreply.github.com>
2020-05-07 15:42:45 +0200

    Update create_tables.sql

--- create_tables.sql
+++ create_tables.sql
@@ -2,7 +2,7 @@ CREATE TABLE clubs (
   id      	int NOT NULL AUTO_INCREMENT, 
   c_name    varchar(255) NOT NULL, 
   country 	varchar(255) NOT NULL, 
-  city  	varchar(255) NOT NULL, 
+  city  	  varchar(255) NOT NULL, 
   PRIMARY KEY (id)
   );
   
@@ -25,7 +25,7 @@ CREATE TABLE disciplines (
   PRIMARY KEY (id));
   
   CREATE TABLE ranks (
-  rank_value 		enum('international','continental','national','usual') NOT NULL,
+  rank_value 		    enum('international','continental','national','usual') NOT NULL,
   rank_description  varchar(255) NOT NULL, 
   PRIMARY KEY (rank_value),
   CHECK (rank_value IN (1,2,3,4))
@@ -43,11 +43,11 @@ CREATE TABLE arenas (
   
   
 CREATE TABLE meetings (
-  id          int NOT NULL AUTO_INCREMENT, 
-  rank_value  enum('international','continental','national','usual') NOT NULL,
-  arena_id    int  NOT NULL, 
-  m_date      date NOT NULL, 
-  m_description varchar(255), 
+  id             int NOT NULL AUTO_INCREMENT, 
+  rank_value     enum('international','continental','national','usual') NOT NULL,
+  arena_id       int  NOT NULL, 
+  m_date         date NOT NULL, 
+  m_description  varchar(255), 
   PRIMARY KEY (id),
   FOREIGN KEY (rank_value) REFERENCES ranks(rank_value),
   FOREIGN KEY (arena_id) REFERENCES arenas(id)
@@ -57,35 +57,35 @@ CREATE TABLE meetings (
   id          	  int NOT NULL AUTO_INCREMENT, 
   discipline_id   varchar(255) NOT NULL, 
   meeting_id      int NOT NULL, 
-  s_type		  enum('final','semi-final','quarter-final','1/8','1/16','elimination') NOT NULL,
+  s_type		      enum('final','semi-final','quarter-final','1/8','1/16','elimination') NOT NULL,
   PRIMARY KEY (id),
   FOREIGN KEY (discipline_id) REFERENCES disciplines(id),
   FOREIGN KEY (meeting_id) REFERENCES meetings(id)
   );
   
   CREATE TABLE jump_results (
-  id 			int NOT NULL AUTO_INCREMENT,
+  id 			    int NOT NULL AUTO_INCREMENT,
   athlete_id 	int NOT NULL,
   series_id 	int NOT NULL,
-  result 		decimal(5,3),
+  result 		  decimal(5,3),
   PRIMARY KEY(id),
   FOREIGN KEY (athlete_id) REFERENCES athletes(id),
   FOREIGN KEY (series_id) REFERENCES series(id)
   );
   
   CREATE TABLE track_results (
-  id 			int NOT NULL AUTO_INCREMENT,
+  id 			    int NOT NULL AUTO_INCREMENT,
   athlete_id 	int NOT NULL,
   series_id		int NOT NULL,
-  result 		time(3),
+  result 		  time(3),
   PRIMARY KEY(id),
   FOREIGN KEY (athlete_id) REFERENCES athletes(id),
   FOREIGN KEY (series_id) REFERENCES series(id)
   );
   
   CREATE TABLE signups (
-  athlete_id		int NOT NULL,
-  meeting_id		int NOT NULL,
+  athlete_id	  	int NOT NULL,
+  meeting_id		  int NOT NULL,
   discipline_id	 	varchar(255) NOT NULL,
   PRIMARY KEY(athlete_id, meeting_id, discipline_id),
   FOREIGN KEY (athlete_id) REFERENCES athletes(id),
@@ -94,10 +94,10 @@ CREATE TABLE meetings (
   );
   
   CREATE TABLE limits (
-  meeting_id 		int NOT NULL,
+  meeting_id 		  int NOT NULL,
   discipline_id 	varchar(255) NOT NULL,
-  track_res 		time(3),
-  jump_res 			decimal(5,3),
+  track_res 		  time(3),
+  jump_res 			  decimal(5,3),
   PRIMARY KEY(meeting_id, discipline_id),
   FOREIGN KEY (meeting_id) REFERENCES meetings(id),
   FOREIGN KEY (discipline_id) REFERENCES disciplines(id)
